3beea4a31a81e57ca4a0a1f149b1fffe3f8e6a7f
controlecidadao <167250480+controlecidadao@users.noreply.github.com>
2024-11-10 03:22:10 -0300

    Add files via upload

--- instalador_main.bat
+++ instalador_main.bat
@@ -1,4 +1,5 @@
 @echo off
+cls
 setlocal
 
 :: Enable UTF-8
@@ -9,120 +10,130 @@ chcp 65001
 title Instalação do SINARC
 
 
-@REM :: Initialize
-@REM :inicio
-@REM cls
-@REM echo.
-@REM echo Bem-vindo(a) à rotina de instalação do Sistema Integrado de Análise de Redes Complexas - SINARC!
-@REM echo.
-@REM echo Deseja instalar o SINARC?
-@REM echo.
-@REM echo 1. Sim
-@REM echo 2. Não
-@REM echo.
-@REM set /p opcao="Selecione a opção (1 ou 2): "
+
+@echo.
+@echo ==============================
+@echo INICIANDO ROTINA DE INSTALAÇÃO
+@echo ==============================
+@echo.
+
+:: Initialize
+:inicio
+echo.
+echo Bem-vindo(a) à rotina de instalação do Sistema Integrado de Análise de Redes Complexas - SINARC!
+echo.
+echo Deseja instalar o SINARC?
+echo.
+echo 1. Sim
+echo 2. Não
+echo.
+set /p opcao="Selecione a opção (1 ou 2): "
+
+:: Input Validation
+if "%opcao%"=="1" (
+    goto continuar
+) else if "%opcao%"=="2" (
+    echo.
+    echo Fechando a rotina de instalação...
+    echo.
+    pause
+    exit
+) else (
+    echo.
+    echo Opção inválida! Por favor, escolha 1 ou 2.
+    echo.
+    pause
+    goto inicio
+)
+
+:continuar
+echo.
+echo Sábia decisão! Continuando a instalação...
+echo.
+pause
 
 
-@REM :: Input Validation
-@REM if "%opcao%"=="1" (
-@REM     goto continuar
-@REM ) else if "%opcao%"=="2" (
-@REM     echo.
-@REM     echo Fechando a rotina de instalação...
-@REM     echo.
-@REM     pause
-@REM     exit
-@REM ) else (
-@REM     echo.
-@REM     echo Opção inválida! Por favor, escolha 1 ou 2.
-@REM     echo.
-@REM     pause
-@REM     goto inicio
-@REM )
+
+:: Section: Miniconda Installation
+@echo.
+@echo.
+@echo.
+@echo ==================================
+@echo OBTENDO CAMINHO DO DIRETÓRIO LOCAL
+@echo ==================================
+@echo.
+@echo Obtendo caminho do diretório local...
+set CURRENT_DIR=%cd%
+@echo Caminho do diretório local: %CURRENT_DIR%
 
 
-@REM :: Installation Steps
-@REM :continuar
-@REM echo.
-@REM echo Sábia decisão! Continuando a instalação...
-@REM echo.
-@REM pause
+
+@echo.
+@echo.
+@echo.
+@echo ====================
+@echo INSTALANDO MINICONDA
+@echo ====================
+@echo.
+
+:: Check if Miniconda directory already exists
+if exist "%CURRENT_DIR%\miniconda3" (
+   echo A pasta 'miniconda3' já existe.
+   echo Removendo a instalação existente...
+   rmdir /s /q "%CURRENT_DIR%\miniconda3"
+   echo Instalação anterior removida. Continuando com a nova instalação...
+)
+
+@echo Criando a pasta "miniconda3" para o Miniconda...
+mkdir miniconda3
+@echo.
+
+:: Download the Miniconda installer with integrity check
+@echo Realizando o download do Miniconda...
+set MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
+curl -L -O %MINICONDA_URL%
+if not exist Miniconda3-latest-Windows-x86_64.exe (
+    echo.
+    echo Erro: Falha no download. Saindo...
+    pause
+    exit /b 1
+)
+
+@echo.
+@echo Verificando integridade do arquivo...
+certutil -hashfile Miniconda3-latest-Windows-x86_64.exe SHA256
+:: Add checksum verification here if the expected hash is known, for example:
+:: set expected_hash=your_expected_hash_here
+:: if not "%hash%"=="%expected_hash%" (
+::     echo.
+::     echo Hash verification failed. Exiting.
+::     exit /b 1
+:: )
+
+@echo.
+@echo Instalando Miniconda em %CURRENT_DIR%\miniconda3...
+start /WAIT "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%CURRENT_DIR%\miniconda3
+if errorlevel 1 (
+    echo.
+    echo Falha na instalação. Saindo...
+    pause
+    exit /b 1
+)
 
 
-@REM :: Section: Miniconda Installation
-@REM @echo.
-@REM @echo.
-@REM @echo.
-@REM @echo ==================================
-@REM @echo OBTENDO CAMINHO DO DIRETÓRIO LOCAL
-@REM @echo ==================================
-@REM @echo.
-@REM @echo Obtendo caminho do diretório local...
-@REM set CURRENT_DIR=%cd%
-@REM @echo Caminho do diretório local: %CURRENT_DIR%
-
-
-@REM :: Check if Miniconda directory already exists
-@REM if exist "%CURRENT_DIR%\miniconda3" (
-@REM    echo.
-@REM    echo A pasta 'miniconda3' já existe.
-@REM    echo Removendo a instalação existente...
-@REM    rmdir /s /q "%CURRENT_DIR%\miniconda3"
-@REM    echo Instalação anterior removida. Continuando com a nova instalação...
-@REM )
-
-@REM @echo.
-@REM @echo.
-@REM @echo.
-@REM @echo ====================
-@REM @echo INSTALANDO MINICONDA
-@REM @echo ====================
-@REM @echo.
-@REM @echo Criando a pasta "miniconda3" para o Miniconda...
-@REM mkdir miniconda3
-@REM @echo.
-
-@REM :: Download the Miniconda installer with integrity check
-@REM @echo Realizando o download do Miniconda...
-@REM set MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-@REM curl -L -O %MINICONDA_URL%
-@REM if not exist Miniconda3-latest-Windows-x86_64.exe (
-@REM     echo.
-@REM     echo Erro: Falha no download. Saindo...
-@REM     pause
-@REM     exit /b 1
-@REM )
-
-@REM @echo.
-@REM @echo Verificando integridade do arquivo...
-@REM certutil -hashfile Miniconda3-latest-Windows-x86_64.exe SHA256
-@REM :: Add checksum verification here if the expected hash is known, for example:
-@REM :: set expected_hash=your_expected_hash_here
-@REM :: if not "%hash%"=="%expected_hash%" (
-@REM ::     echo.
-@REM ::     echo Hash verification failed. Exiting.
-@REM ::     exit /b 1
-@REM :: )
-
-@REM @echo.
-@REM @echo Instalando Miniconda em %CURRENT_DIR%\miniconda3...
-@REM start /WAIT "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%CURRENT_DIR%\miniconda3
-@REM if errorlevel 1 (
-@REM     echo.
-@REM     echo Falha na instalação. Saindo...
-@REM     pause
-@REM     exit /b 1
-@REM )
-
-
-@REM :: Clean up installer
-@REM @echo Removendo arquivo de instalação...
-@REM DEL Miniconda3-latest-Windows-x86_64.exe
-@REM @echo Instalação do Miniconda realizada com sucesso!
-@REM @echo.
+:: Clean up installer
+@echo Removendo arquivo de instalação...
+DEL Miniconda3-latest-Windows-x86_64.exe
+@echo Instalação do Miniconda realizada com sucesso!
+@echo.
 
 @REM pause
 @REM exit
+
+
+
+
+
 
 setlocal EnableDelayedExpansion
 
@@ -147,13 +158,13 @@ if errorlevel 1 (
     goto :ERROR
 )
 
-:: Inicia download
+:: Inicia download 
 echo.
 echo Baixando arquivo master.zip do repositório REDE CNPJ...
-echo  - Fonte: Github (waltervix/rede-cnpj)
+echo  - Fonte: Github (controlecidadao/rede-cnpj)
 
 :: Tenta realizar o download
-curl --insecure -L https://github.com/waltervix/rede-cnpj/archive/refs/heads/master.zip -o master.zip
+curl --insecure -L https://github.com/controlecidadao/rede-cnpj/archive/refs/heads/master.zip -o master.zip
 if errorlevel 1 (
     echo Erro: Falha ao baixar o arquivo master.zip
     goto :ERROR
@@ -195,6 +206,14 @@ exit /b 1
 @REM endlocal
 @REM exit /b 0
 
+
+@REM pause
+@REM exit
+
+
+
+
+
 echo.
 echo.
 echo.
@@ -203,17 +222,17 @@ echo DOWNLOAD DO REPOSITÓRIO SINARC
 echo ==============================
 echo.
 
-@REM :: Inicia download
-@REM echo.
-@REM echo Baixando arquivo main.zip do repositório SINARC...
-@REM echo  - Fonte: Github (waltervix/sinarc)
+:: Inicia download
+echo.
+echo Baixando arquivo main.zip do repositório SINARC...
+echo  - Fonte: Github (controlecidadao/sinarc)
 
-@REM :: Tenta realizar o download
-@REM curl --insecure -L https://github.com/waltervix/sinarc/archive/refs/heads/main.zip -o main.zip
-@REM if errorlevel 1 (
-@REM     echo Erro: Falha ao baixar o arquivo main.zip
-@REM     goto :ERROR
-@REM )
+:: Tenta realizar o download
+curl --insecure -L https://github.com/controlecidadao/sinarc/archive/refs/heads/main.zip -o main.zip
+if errorlevel 1 (
+    echo Erro: Falha ao baixar o arquivo main.zip
+    goto :ERROR
+)
 
 :: Verifica se o arquivo foi baixado
 if not exist main.zip (
@@ -237,7 +256,6 @@ if errorlevel 1 (
 )
 
 
-echo.
 echo Operação concluída com sucesso!
 echo.
 @REM pause
@@ -259,6 +277,17 @@ exit /b 1
 
 
 
+@REM echo.
+@REM echo.
+@REM echo.
+@REM echo =============================
+@REM echo INSTALANDO SINARC E REDE CNPJ
+@REM echo =============================
+
+@REM echo Instalando SINARC e REDE CNPJ...
+
+
+
 @echo.
 @echo.
 @echo.
@@ -267,9 +296,10 @@ exit /b 1
 @echo ================================================
 @echo.
 
-:: Copia o arquivo 'sinarc-main\rede.ini' para a pasta 'sinarc-main\rede-cnpj-master\rede', substituindo o lá existente
+:: Copia o arquivo 'sinarc-main\rede.ini' para a pasta 'rede-cnpj-master\rede', substituindo o lá existente
 @echo Copiando arquivo 'rede.ini' para pasta 'rede-cnpj-master\rede'
-copy %cd%\sinarc-main\rede.ini %cd%\..\rede-cnpj-master\rede
+copy %cd%\sinarc-main\rede.ini %cd%\rede-cnpj-master\rede
+
 
 ::@echo.
 ::@echo.
@@ -330,12 +360,11 @@ copy %cd%\sinarc-main\cria_ambiente_sinarc.txt %cd%\sinarc-main\cria_ambiente_si
 
 @REM call %CURRENT_DIR%\sinarc-main\instala_miniconda.bat
 
+echo Criando ambiente virtual rede_cnpj...
 call %cd%\sinarc-main\cria_ambiente_rede_cnpj.bat
 
-pause
-exit
-
-@REM call %cd%\sinarc-main\cria_ambiente_sinarc.bat
+echo Criando ambiente virtual sinarc...
+call %cd%\sinarc-main\cria_ambiente_sinarc.bat
 
 
 @echo.
